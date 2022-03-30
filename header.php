b3029faec7dc819984b84d7eb5431bebcf1c55f3
Ismaël Zerrouk <zerroukism@cy-tech.fr>
2022-03-30 21:11:06 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,19 @@
+<?php
+echo '<header class="entete">
+    <h1 id="logo"><a href="index.html"> FitProt </a></h1>
+        <nav>
+            <ul>
+                <li><a href="index.html">Accueil</a></li>
+                <li class="deroulant"><a href="nutrition.html">Nutrition</a></li>
+
+                <li class="deroulant"><a href="vetements.html">Vêtements</a></li>
+
+                <li><a href="accessoires.html">Accessoires</a></li>
+                <li><a href="contact.html">Contactez nous</a></li>
+            </ul>
+
+        </nav>
+        <div class="compte"><a href="#"><i class="far fa-user"></i> Se connecter </a></div>
+        <div class="compte"><a href="#"><i class="fas fa-shopping-basket"></i>Panier</a></div>
+        </header>
+';
